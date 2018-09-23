a0d9830409ec5aa7152082eb7a54011ed177dd15
LynxZh <phoenixsbk@gmail.com>
2018-09-22 19:30:36 -0700

    Init

---
+++ src/main/java/com/fluffynx/fluffiegallery/FluffiegalleryApplication.java
@@ -0,0 +1,12 @@
+package com.fluffynx.fluffiegallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffiegalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluffiegalleryApplication.class, args);
+	}
+}
